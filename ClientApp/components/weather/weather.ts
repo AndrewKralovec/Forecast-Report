e80c864b1d9155262477a2a6f4057007d7ef4e60
AndrewKralovec <akrala@siu.edu>
2016-08-30 14:04:37 -0500

    update

--- ClientApp/components/weather/weather.ts
+++ ClientApp/components/weather/weather.ts
@@ -15,76 +15,26 @@ import { ForcastService } from '../../services/forecastService.ts';
 export class Weather {
     private data:any; 
     private forecast:Forecast; 
+    // Default location coordniates 
     private location:Location = {
         latitude:41.8093699,
         longitude:-89.8093699
     }; 
-    private days = [
-        { title:"Monday",temp:25,info:"Going to be cold" },
-        { title:"Tuesday",temp:35,info:"Going to be cold" },
-        { title:"Wednesday",temp:45,info:"Going to be cold" },
-        { title:"Thursday",temp:55,info:"Going to be cold" },
-        { title:"Friday",temp:65,info:"Going to be cold" },
-        { title:"Saturday",temp:75,info:"Going to be cold" },
-        { title:"Sunday",temp:85,info:"Going to be cold" },
-    ]; 
-    
     constructor(public fs:ForcastService){
-        // Geolocation.
-        // this.refresh(); 
         fs.getForcast(this.location.latitude,this.location.longitude)
         .subscribe(result => {
             this.forecast = result ;
             console.log(this.forecast);
         }); 
     }
-    Debug(data:any){
-        console.log("Data"); 
-        this.data = data; 
-        console.log(this.data); 
-    }
     search(latitude: any, longitude: any){
         this.fs.getForcast(latitude,longitude)
-            .subscribe(
-                data =>  this.Debug(data),
-                err =>  console.log("Error: \n"+err),
-                () => console.log('Get Complete')
-            ); 
+            .subscribe(result => {
+                this.forecast = result ;
+                console.log(this.forecast);
+            });
     }
     dateFormat(unix:number){
         return new Date(unix * 1000); 
     }
-    refresh(){
-        console.log("Refresh");
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.showPosition, this.showError); 
-        } else {
-          console.log("Browser doesn't support Geolocation"); 
-        }
-    }
-    showPosition(position) {
-        var pos = {
-            latitude:position.coords.latitude,
-            longitude:position.coords.longitude
-        };
-        console.log("Postion: "+pos.latitude+" "+pos.longitude); 
-    }
-
-    showError(error) {
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-                console.log("User denied the request for Geolocation."); 
-                break;
-            case error.POSITION_UNAVAILABLE:
-                console.log("Location information is unavailable.");
-                break;
-            case error.TIMEOUT:
-                console.log("The request to get user location timed out.");
-                break;
-            case error.UNKNOWN_ERROR:
-                console.log("An unknown error occurred.");
-                break;
-        }
-    }
-    
 }
