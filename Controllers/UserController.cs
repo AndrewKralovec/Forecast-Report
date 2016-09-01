73311aa337e87a0d071dd5eb91620428f78152ed
AndrewKralovec <akrala@siu.edu>
2016-09-01 14:39:46 -0500

    login

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.Data.Sqlite.Utilities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlueWolf.Controllers
 {
+    [Route("api/[controller]")]
     public class UserController : Controller
     {
-        
+        private string cs {get;set;}
+        private string table {get;set;}
+        public UserController()
+        {
+            this.cs = "Data Source=/home/andrew/Elearn.db"; 
+        }
+        [HttpPost("[action]")]
+        public int find(string un, string pwd){
+            int count=0; 
+            using(SqliteConnection con = new SqliteConnection(cs))
+            {
+                con.Open();
+                string stm = $"SELECT count(*) FROM Accounts WHERE USERNAME='{un}' AND PASSWORD='{pwd}'";
+                if(pwd == null){
+                    stm = $"SELECT count(*) FROM Accounts WHERE USERNAME='{un}'";
+                }
+                
+                using (SqliteCommand cmd = new SqliteCommand(stm, con))
+                {
+                    count = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+
+                con.Close();   
+            }
+            return count ; 
+        }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueWolf.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
