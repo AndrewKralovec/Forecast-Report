94d2686a415e2a3c1c5bbfe8cd4a4efed58322b8
AndrewKralovec <akrala@siu.edu>
2016-09-06 01:37:45 -0500

    Working

--- ClientApp/components/home/home.ts
+++ ClientApp/components/home/home.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { LoginService } from '../../services/login.service';
-import { ForecastService } from '../../services/forecast.service';
 
 @Component({
   selector: 'home',
-  providers: [LoginService, ForecastService],
   template: require('./home.html')
 })
 export class Home {
