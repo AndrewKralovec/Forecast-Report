5a4e841367f9ceb10fb6e853acee359f727f1d4f
AndrewKralovec <akrala@siu.edu>
2016-09-08 12:09:14 -0500

    tests

---
+++ Test/ForecastControllerTests.cs
@@ -0,0 +1,24 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
+using BlueWolf.Models; 
+using BlueWolf.Controllers; 
+
+namespace BlueWolf.Test  
+{
+    public class ForecastControllerTests
+    {
+        [Fact]
+        public void IndexReturnsView()
+        {
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.Equal("Home Page",result.ViewData["Title"]);
+        }
+    }
+}
