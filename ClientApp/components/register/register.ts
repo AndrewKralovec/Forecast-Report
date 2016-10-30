4c4d61e98bd060a5262d8c4424f42928480afe97
AndrewKralovec <akrala@siu.edu>
2016-10-29 19:23:53 -0500

    Identity Added

--- ClientApp/components/register/register.ts
+++ ClientApp/components/register/register.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
 import { CORE_DIRECTIVES, FORM_DIRECTIVES } from '@angular/common';
 import { LoginService } from '../../services/login.service';
-import { User } from '../../models/user';
 // import {FormGroup, FormControl, FormBuilder, Validators, REACTIVE_FORM_DIRECTIVES} from "@angular/forms";
 
 @Component({
     selector: 'login',
     directives: [FORM_DIRECTIVES],
     providers: [LoginService],
-    template: require('./login.html'),
-    styleUrls:['./styles/login.css']
+    template: require('./register.html'),
+    styleUrls:['./styles/register.css']
 })
-export class Login {
+export class Register {
     constructor(private ls:LoginService){
     }
-    // Login in user
-    login(email: any, password: any){
-        this.ls.find(email,password); 
+    // Register user
+    register(email: any, password: any, confirm: any){
+        this.ls.register(email, password, confirm); 
     }
 }
